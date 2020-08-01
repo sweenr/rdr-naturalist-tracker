10514752ce93098c96dabd9fd997dbc0a11daf3a
Richard Sween <sweenr418@gmail.com>
2020-08-01 17:21:26 -0500

    Finish adding habitats and animals, add habitat images

--- src/components/habitat.js
+++ src/components/habitat.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Habitat = (props) => (
-  <a href={`/habitat/${props.id}`} className="habitat card">
-    {/* TODO add image */}
-    <h2>{props.title}</h2>
-    <p>{props.description}</p>
-    <p>{`Stamped: ${props.stamped} of ${props.total}`}</p>
-  </a>
-);
+const Habitat = (props) => {
+  const image = require(`../images/habitats/${props.id}.jpg`);
+  return (
+    <a href={`/habitat/${props.id}`} className="habitat card">
+      {/* TODO add image */}
+      <img src={image} alt={`${props.title} scenery`} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <p>{`Stamped: ${props.stamped} of ${props.total}`}</p>
+    </a>
+  );
+};
 
 export default Habitat;
