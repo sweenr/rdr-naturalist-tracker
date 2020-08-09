813d02240b2017c86a283f8285e8c5e544f2e107
Richard Sween <sweenr418@gmail.com>
2020-08-09 16:00:08 -0500

    Make habitat urls relative

--- src/components/habitat.js
+++ src/components/habitat.js
@@ -3,7 +3,7 @@ import React from "react";
 const Habitat = (props) => {
   const image = require(`../images/habitats/${props.id}.jpg`);
   return (
-    <a href={`/habitat/${props.id}`} className="habitat card">
+    <a href={`habitat/${props.id}`} className="habitat card">
       <img src={image} alt={`${props.title} scenery`} />
       <h2>{props.title}</h2>
       <p className="description">{props.description}</p>
